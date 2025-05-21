37f6b62f013694c1cfcf3786c0c11c496cb30128
Daniel Oviedo <danielovi18@hotmail.com>
2025-05-21 11:25:01 -0600

    AvanceDia0

---
+++ src/clases/Personas.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Personas {
+
+    public String nombre;
+    public int edad;
+
+    public void mostrarDatos(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+
+    }
+
+
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+import clases.Personas;
+public class Main {
+    public static void main(String[] args) {
+
+        Personas persona1 = new Personas();
+        persona1.nombre = "Daniel Oviedo";
+        persona1.edad = 20;
+        persona1.mostrarDatos();
+
+        Personas persona2 = new Personas();
+        persona2.nombre = "Adolf Hitler";
+        persona2.edad = 500;
+        persona2.mostrarDatos();
+
+
+
+
+
+    }
+}
